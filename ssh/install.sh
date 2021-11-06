5025ff648b00cf39fa888efdda6a42595e7357b0
Hu Peng <ko.h@ctw.inc>
2021-11-07 01:39:14 +0900

    fix mkdir ssh

--- ssh/install.sh
+++ ssh/install.sh
@@ -3,11 +3,11 @@ BASE_DIR=$HOME/.xiaomo/source
 echo 🍗 开始配置ssh
 cp $BASE_DIR/ssh/id_rsa* $HOME/.ssh
 cp $BASE_DIR/ssh/*.pem $HOME/.ssh
-mkdir -p ~/.ssh
-chmod 700 ~/.ssh
-touch ~/.ssh/authorized_keys
-chmod 600 ~/.ssh/authorized_keys
+mkdir -p $HOME/.ssh
+chmod 700 $HOME/.ssh
+touch $HOME/.ssh/authorized_keys
+chmod 600 $HOME/.ssh/authorized_keys
 
 echo 🍗 开始写入授权密钥
-cat ~/.ssh/id_rsa.pub >>authorized_keys
-cat ~/.ssh/authorized_keys
+cat $HOME/.ssh/id_rsa.pub >>authorized_keys
+cat $HOME/.ssh/authorized_keys
