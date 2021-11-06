50ce1c6abdf657eb6d06cf36413127c82c4954ba
Hu Peng <ko.h@ctw.inc>
2021-11-07 01:50:53 +0900

    change position

--- ssh/install.sh
+++ ssh/install.sh
@@ -1,9 +1,9 @@
 BASE_DIR=$HOME/.xiaomo/source
 
 echo 🍗 开始配置ssh
+mkdir -p $HOME/.ssh
 cp $BASE_DIR/ssh/id_rsa* $HOME/.ssh
 cp $BASE_DIR/ssh/*.pem $HOME/.ssh
-mkdir -p $HOME/.ssh
 chmod 700 $HOME/.ssh
 touch $HOME/.ssh/authorized_keys
 chmod 600 $HOME/.ssh/authorized_keys
