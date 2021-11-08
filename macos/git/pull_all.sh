a1c66f72eb21e60e046ef9e4877c9c24fec03c5d
suzukaze hazuki <suzukaze.hazuki2020@gmail.com>
2021-11-08 12:16:18 +0900

    pull all

---
+++ macos/git/pull_all.sh
@@ -0,0 +1,11 @@
+# shellcheck disable=SC2045
+for dir in $(ls .); do
+  if [ -d "$dir" ]; then
+    echo "$dir"
+    # shellcheck disable=SC2164
+    cd "$dir"
+    git pull
+    # shellcheck disable=SC2103
+    cd ..
+  fi
+done
