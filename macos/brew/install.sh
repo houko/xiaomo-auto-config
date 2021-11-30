1a8a09f672a7eeb9f3b0b0368dff3254754af4d1
Evan <suzukaze.hazuki2020@gmail.com>
2021-11-30 15:18:02 +0900

    fix brew

--- macos/brew/install.sh
+++ macos/brew/install.sh
@@ -2,9 +2,8 @@ echo 🍁 开始安装brew
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 #echo 🍁 添加eval到zprofile和shellenv
-#echo "eval '$(/usr/local/Homebrew shellenv)'" >>"$HOME"/.zprofile
-#eval "$(/usr/local/Homebrew shellenv)
-#source "$HOME"/.zprofile
+echo "eval '$(/usr/local/Homebrew shellenv)'" >>"$HOME"/.zprofile
+source "$HOME"/.zprofile
 
 echo 🍁 添加cask和cask-fonts源
 brew tap homebrew/cask
