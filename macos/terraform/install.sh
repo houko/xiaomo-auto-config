5de11dd236dbc47a1383e68685713f9e46a8176f
Evan <suzukaze.hazuki2020@gmail.com>
2021-12-20 08:05:09 +0900

    upgrade terraform version to v1.1.2

--- macos/terraform/install.sh
+++ macos/terraform/install.sh
@@ -1,11 +1,11 @@
 echo 🍧 开始安装terraform多版本管理工具tfenv
 brew install tfenv
 
-echo 🍧 开始安装terraform1.0.10
-tfenv install 1.0.10
+echo 🍧 开始安装terraform1.1.2
+tfenv install 1.1.2
 
 echo 🍧 设置默认版本为1.0.10
-tfenv use 1.0.10
+tfenv use 1.1.2
 
 echo 🍧 当前terraform版本
 terraform -v
